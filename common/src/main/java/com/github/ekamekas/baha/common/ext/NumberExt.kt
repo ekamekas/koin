f08cd819bbc774dc001f3800590c0bbed6b865e4
Mas Eka Setiawan <engineer.laman.id@gmail.com>
2020-07-18 14:58:54 +0700

    [UPDATE] Dashboard layout

--- common/src/main/java/com/github/ekamekas/baha/common/ext/NumberExt.kt
+++ common/src/main/java/com/github/ekamekas/baha/common/ext/NumberExt.kt
@@ -2,6 +2,7 @@ package com.github.ekamekas.baha.common.ext
 
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 /**
  * Map epoch to string representation
@@ -17,4 +18,29 @@ fun Long.toTime(locale: String = "en", format: String = "HH:mm"): String {
 fun Long.toDate(locale: String = "en", format: String = "dd MMM yyyy"): String {
     return SimpleDateFormat(format, Locale(locale))
         .format(Date(this))
+}
+
+/**
+ * Map epoch to string representation
+ */
+fun Long.toDaysOfTheWeekOrDate(locale: String = "en", format: String = "dd MMM yyyy"): String {
+    return when(TimeUnit.MILLISECONDS.toDays(System.currentTimeMillis() - this)) {
+        -1L -> "Tomorrow"
+        0L -> "Today"
+        1L -> "Yesterday"
+        else -> toDate(locale, format)
+    }
+}
+
+/**
+ * Map millis to epoch day
+ */
+fun Long.toEpochDay(): Long {
+    return Calendar.getInstance().apply {
+        timeInMillis = this@toEpochDay
+        set(Calendar.HOUR_OF_DAY, 0)
+        set(Calendar.MINUTE, 0)
+        set(Calendar.SECOND, 0)
+        set(Calendar.MILLISECOND, 0)
+    }.timeInMillis
 }
