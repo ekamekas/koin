a59128bfc278b7a81bebd8cb2c2d673a384d1c87
Mas Eka Setiawan <engineer.laman.id@gmail.com>
2020-08-08 00:00:06 +0700

    [UPDATE] Transaction record launcher shortcut

--- app/src/main/java/com/github/ekamekas/koin/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/github/ekamekas/koin/presentation/splash/SplashActivity.kt
@@ -2,9 +2,11 @@ package com.github.ekamekas.koin.presentation.splash
 
 import android.content.Intent
 import android.os.Handler
+import androidx.core.app.TaskStackBuilder
 import com.github.ekamekas.baha.core.presentation.activity.BaseActivity
 import com.github.ekamekas.koin.R
 import com.github.ekamekas.koin.presentation.dashboard.DashboardActivity
+import com.github.ekamekas.koin.transaction.presentation.transaction_record.TransactionRecordActivity
 
 /**
  * The main entry activity, this activity should launch from the app launcher
@@ -28,7 +30,24 @@ class SplashActivity: BaseActivity() {
 
     // callback
     private fun onSplashTimeOut() {
-        delegateDashboard()
+        ifStartByLauncherElse(
+            ifLaunched = {
+                delegateDashboard()
+            },
+            ifElse = {
+                onStartByShortcut()
+            }
+        )
+    }
+    private fun onStartByShortcut() {
+        when(intent.action) {
+            "ADD_TRANSACTION" -> {
+                delegateAddTransactionCategory()
+            }
+            else -> {
+                delegateDashboard()
+            }
+        }
     }
 
     // delegate
@@ -40,6 +59,30 @@ class SplashActivity: BaseActivity() {
         }.also {
             finish()
         }
+    }
+    private fun delegateAddTransactionCategory() {
+        TaskStackBuilder.create(this)
+            .addNextIntent(
+                Intent(this, DashboardActivity::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+                }
+            )
+            .addNextIntent(
+                Intent(this, TransactionRecordActivity::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+                }
+            )
+            .startActivities()
+            .also {
+                finish()
+            }
+    }
 
+    private fun ifStartByLauncherElse(ifLaunched: () -> Unit, ifElse: () -> Unit) {
+        if(intent.action == Intent.ACTION_MAIN && intent.hasCategory(Intent.CATEGORY_LAUNCHER)) {
+            ifLaunched.invoke()
+        } else {
+            ifElse.invoke()
+        }
     }
 }
